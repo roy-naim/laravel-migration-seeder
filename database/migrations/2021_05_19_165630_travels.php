1ceb09c9203341357697df0c2f4c918d43fcc829
roy-naim <b.roynaim@gmail.com>
2021-05-19 20:41:29 +0300

    migration updated

--- database/migrations/2021_05_19_165630_travels.php
+++ database/migrations/2021_05_19_165630_travels.php
@@ -13,7 +13,18 @@ class Travels extends Migration
      */
     public function up()
     {
-        //
+      Schema::create('travels', function (Blueprint $table)
+      {
+        $table->bigIncrements('id');
+        $table->string('city');
+        $table->string('state');
+        $table->string('airline');
+        $table->string('hotel_name');
+        $table->tinyInteger('rooms')->unsigned();
+        $table->string('european_citizen', true);
+        $table->smallInteger('price');
+
+      });
     }
 
     /**
